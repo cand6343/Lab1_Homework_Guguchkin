e1247bbda97a60282c7dd3068d245c7b24d95031
cand6123 <fjhdjdj876@gmail.com>
2025-10-16 19:20:05 +0300

    2 задание

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 
-Console.Write("Введите первое целое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое целое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе целое число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе целое число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-int sum = num1 + num2;
-int diff = num1 - num2;
-int prod = num1 * num2;
-double quot = (double)num1 / num2;
+// int sum = num1 + num2;
+// int diff = num1 - num2;
+// int prod = num1 * num2;
+// double quot = (double)num1 / num2;
 
-Console.WriteLine(sum);
-Console.WriteLine(diff);
-Console.WriteLine(prod);
-Console.WriteLine(quot);
+// Console.WriteLine(sum);
+// Console.WriteLine(diff);
+// Console.WriteLine(prod);
+// Console.WriteLine(quot);
+
+//2 задание
+
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+
+Console.Write("Введите возраст: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.Write("Введите любимый язык программирования: ");
+string language = Console.ReadLine();
+
+Console.WriteLine($"Привет, {name}!");
+Console.WriteLine($"Тебе {age} лет.");
+Console.WriteLine($"Твой любимый язык программирования — {language}");
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce46696206cc93b5c0661655dad1baa4a876ddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2367b21c8fc7d7a68e11b12305d7cfc41235c86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
